9baba2c159e4d8c21a771f950e28f73ddd9b54ef
顾士成 <584268168@qq.com>
2021-03-13 22:24:26 +0800

    new project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left"></div>
+    <div class="right"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang='scss' scoped>
+.home{
+  height: 100%;
+  display: flex;
+  .left{
+    width: 200px;
+    background: aqua;
+    height: 100%
+  }
+  .right{
+
+
+
+
+
+    flex: 1;
+    background: red;
+  }
+}
+
+</style>
